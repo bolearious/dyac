21332e5d463748a42181d18b51136c2731d901dc
Bryan OLeary <boleary@clipsyndicate.com>
2013-09-24 00:42:19 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dyac::Application.config.secret_token = 'dc1abb344b608f6cfbe34de52857faaa71a63c9f7947ad098512717a6f3b09fe722939053dc007fa77196ca729b0a5329c979be63d3269de01f58f796971c9b2'
